e5c987612022ceb39ac13e8ae9eaf54e915b8eef
chldudqlsdl <chldudqlsdl@naver.com>
2022-06-26 21:58:47 +0900

    두번째

---
+++ CLASS 2/#10845 큐.py
@@ -0,0 +1,30 @@
+import sys
+
+N = int(sys.stdin.readline())
+queue = []
+for _ in range(N):
+    order = sys.stdin.readline().split()
+    if order[0] == 'push':
+        queue.append(int(order[1]))
+    elif order[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.pop(0))
+    elif order[0] == 'size':
+        print(len(queue))
+    elif order[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif order[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    elif order[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
